276e1cec5f80525573bb0446db1aee455491a006
Adam <adamtroxler@gmail.com>
2017-06-02 00:32:37 +0000

    Adam and Zachary did it! Yay!

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 
 """This is the entry point of the program."""
+from collections import defaultdict
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    
+    count = defaultdict(int)
+    # create empty dict with default count at 0
+    
+    text = text.lower().split()
+    # lowercase and split strings
+    
+    for word in text:
+        for language in languages:
+        # for each language in the language list
+            for item in language['common_words']:
+            # for each word in the common_words list
+                if item == word:
+                    count[language['name']] += 1
+                    # pass the language to the count dictionary and increment the count
+                    
+    maxWord = 0
+    maxLanguage = 0
+    for language, total in count.items():
+        if total > maxWord:
+            maxWord = total
+            maxLanguage = language
+    return maxLanguage
+   
